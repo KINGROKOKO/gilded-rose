c9b4b1c9e7ba8733afa016306f0e1a590c48e53b
Thomas Ikimi <themerc01@gmail.com>
2017-10-31 16:27:20 +0000

    Aged Brie and Backstage Pass increase in value tests passing

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -26,18 +26,18 @@ describe GildedRose do
       GildedRose.new(items).update_quality
       expect(items[0].sell_in).to eq(1)
     end
+
+    it "Aged Brie increases in quality" do
+      items = [Item.new("Aged Brie", 1, 1)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq(2)
+    end
     #
-    # it "does not change the name" do
-    #   items = [Item.new("fixme", 0, 0)]
-    #   GildedRose.new(items).update_quality()
-    #   expect(items[0].name).to eq "fixme"
-    # end
-    #
-    # it "does not change the name" do
-    #   items = [Item.new("fixme", 0, 0)]
-    #   GildedRose.new(items).update_quality()
-    #   expect(items[0].name).to eq "fixme"
-    # end
+    it "Backstage Pass increases in quality" do
+      items = [Item.new("Aged Brie", 1, 1)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq(2)
+    end
   end
 
 end
